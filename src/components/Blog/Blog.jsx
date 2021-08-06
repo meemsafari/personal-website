b90e8e37e0d26fa271d3c54c85e3fd172071dc44
Mahdi <meemsafari@gmail.com>
2021-08-06 15:07:13 +0430

    load blog data with useEffect and add loading

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "../Layout/Layout";
 import POSTS from "../App/POSTS.json";
 import PostItem from "../PostItem/PostItem";
 
 const Blog = () => {
+
+	const [posts, handlePosts] = useState([]);
+	const [loading, handleLoading] = useState(false);
+
+	useEffect(() => {
+
+		handleLoading(true);
+		handlePosts(POSTS); // ToDo: need use API
+		handleLoading(false);
+
+	}, [])
+
 	return (
 		<Layout>
 			<div className="container">
@@ -13,7 +25,10 @@ const Blog = () => {
 							Blog
 						</h1>
 						<div className="row">
-							{POSTS.map((item) => (
+							{/* ToDo: me: Make component for loading */}
+							{loading && <div className="col-12 text-center pt-5
+							pb-5">loading</div>}
+							{posts && !loading && posts.map((item) => (
 								<PostItem item={item} key={item.id} />
 							))}
 						</div>
